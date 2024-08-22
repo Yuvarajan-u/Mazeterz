d6210cbb98af51e0e5af30bb1ae482c1295a6e11
Yuvarajan <yuvarajanu123@gmail.com>
2024-08-23 04:38:09 +0530

    frontend fix

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import datetime
-from source.model import seat_availability
 
 app = Flask(__name__)
 
@@ -9,41 +8,41 @@ def index():
     if request.method == 'POST':
         department_name = request.form['department']
         today = datetime.datetime.now().strftime('%A')
-        result = seat_availability(today)
-        # sai and tsi need to be removed
+
+        # Example result for the current day
         result = {
-            'Monday': {'DWA': 7, 'PTL': 5, 'CRR': 3, 'MOP': 0}
+            'Friday': {'DWA': 1, 'PTL': 5, 'CRR': 3, 'MOP': 0}
         }
         department_seats = {
-            'DWA': 197, 
-            'PTL': 201,  
-            'MOP': 96,  
-            'CRR': 82 
+            'DWA': 197,
+            'PTL': 201,
+            'MOP': 96,
+            'CRR': 82
         }
+
         prediction = {}
         failure_lambda = {}
         sorted_departments = {}
-        for day, value in result.items():
-            if day == today:
-                if value.get(department_name) >2:
-                    prediction["success"] = "you can find seat in your own bay"
-                else:
-                    for dept, probability in value.items():
-                        if (dept == department_name):
-                            continue
-                        else:
-                            failure_lambda["dept"] = float(probability / department_seats.get(dept))      
-                    sorted_departments = sort_departments_by_value(failure_lambda)
-                    prediction["failure"] = sorted_departments
+
+        if today in result:
+            value = result[today]
+            if value.get(department_name, 0) > 2:
+                prediction["success"] = "You can find a seat in your own bay"
+            else:
+                for dept, probability in value.items():
+                    if dept != department_name:
+                        failure_lambda[dept] = float(probability / department_seats.get(dept))
+                sorted_departments = sort_departments_by_value(failure_lambda)
+                prediction["failure"] = sorted_departments
 
         return jsonify(department=department_name, result=prediction)
+
     return render_template('index.html')
 
 def sort_departments_by_value(department_dict):
     # Sort the dictionary by values
     sorted_departments = dict(sorted(department_dict.items(), key=lambda item: item[1]))
     return sorted_departments
-
 
 if __name__ == '__main__':
     app.run(debug=True)
