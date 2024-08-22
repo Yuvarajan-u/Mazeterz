8c29663cab037805f8e1b9f7a3fd1c428c404586
Yuvarajan <yuvarajanu123@gmail.com>
2024-08-23 04:41:56 +0530

    front end fix 2

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ def index():
         today = datetime.datetime.now().strftime('%A')
 
         # Example result for the current day
-        result = {
-            'Friday': {'DWA': 1, 'PTL': 5, 'CRR': 3, 'MOP': 0}
-        }
-        department_seats = {
+        result = {'DWA': 1, 'PTL': 5, 'CRR': 3, 'MOP': 0}
+        department_seats = { 
             'DWA': 197,
             'PTL': 201,
             'MOP': 96,
@@ -24,16 +22,15 @@ def index():
         failure_lambda = {}
         sorted_departments = {}
 
-        if today in result:
-            value = result[today]
-            if value.get(department_name, 0) > 2:
-                prediction["success"] = "You can find a seat in your own bay"
-            else:
-                for dept, probability in value.items():
-                    if dept != department_name:
-                        failure_lambda[dept] = float(probability / department_seats.get(dept))
-                sorted_departments = sort_departments_by_value(failure_lambda)
-                prediction["failure"] = sorted_departments
+
+        if result.get(department_name, 0) > 2:
+            prediction["success"] = "You can find a seat in your own bay"
+        else:
+            for dept, probability in result.items():
+                if dept != department_name:
+                    failure_lambda[dept] = float(probability / department_seats.get(dept))
+            sorted_departments = sort_departments_by_value(failure_lambda)
+            prediction["failure"] = sorted_departments
 
         return jsonify(department=department_name, result=prediction)
 
